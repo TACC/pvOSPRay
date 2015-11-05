cec35fad2b93bb0113c9f68dd1c7b32519db22b1
carsonbrownlee <carson@login3.stampede.tacc.utexas.edu>
2015-11-04 19:46:01 -0600

    revert to 836c814758127e1fede695fc7f51b54c26f80b34

--- ParaView/vtkPVOSPRayView.cxx
+++ ParaView/vtkPVOSPRayView.cxx
@@ -1,18 +1,18 @@
-/* =======================================================================================
-   Copyright 2014-2015 Texas Advanced Computing Center, The University of Texas at Austin
+/* ======================================================================================= 
+   Copyright 2014-2015 Texas Advanced Computing Center, The University of Texas at Austin  
    All rights reserved.
-
-   Licensed under the BSD 3-Clause License, (the "License"); you may not use this file
-   except in compliance with the License.
-   A copy of the License is included with this software in the file LICENSE.
-   If your copy does not contain the License, you may obtain a copy of the License at:
-
-       http://opensource.org/licenses/BSD-3-Clause
-
-   Unless required by applicable law or agreed to in writing, software distributed under
-   the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
-   KIND, either express or implied.
-   See the License for the specific language governing permissions and limitations under
+                                                                                           
+   Licensed under the BSD 3-Clause License, (the "License"); you may not use this file     
+   except in compliance with the License.                                                  
+   A copy of the License is included with this software in the file LICENSE.               
+   If your copy does not contain the License, you may obtain a copy of the License at:     
+                                                                                           
+       http://opensource.org/licenses/BSD-3-Clause                                         
+                                                                                           
+   Unless required by applicable law or agreed to in writing, software distributed under   
+   the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY 
+   KIND, either express or implied.                                                        
+   See the License for the specific language governing permissions and limitations under   
    limitations under the License.
 
    pvOSPRay is derived from VTK/ParaView Los Alamos National Laboratory Modules (PVLANL)
@@ -29,13 +29,13 @@
 #include "vtkOSPRayRenderer.h"
 #include "vtkObjectFactory.h"
 #include "vtkPVAxesWidget.h"
-#include "vtkGenericRenderWindowInteractor.h"
+#include "vtkPVGenericRenderWindowInteractor.h"
 #include "vtkPVSynchronizedRenderer.h"
 #include "vtkRenderViewBase.h"
 
 #include "vtkQtProgressiveRenderer.h"
 #include "vtkCommand.h"
-//#include "vtkPVGenericRenderWindowInteractor.h"
+#include "vtkPVGenericRenderWindowInteractor.h"
 
 static void RenderUpdateCallback(void* pvView)
 {
@@ -160,7 +160,7 @@ void vtkPVOSPRayView::SetEnableProgressiveRefinement(int newval)
     if (this->Interactor)
     {
       if (newval)
-        ProgressiveRenderer->resumeAutoUpdates();
+        ProgressiveRenderer->resumeAutoUpdates();        
       else
         ProgressiveRenderer->stopAutoUpdates();
     }
