55d1d45638635d508faebed79311d233cd456225
carsonbrownlee <carson@login1.stampede.tacc.utexas.edu>
2015-12-18 17:38:51 -0600

    removing glclear

--- VTK/vtkOSPRayRenderer.cxx
+++ VTK/vtkOSPRayRenderer.cxx
@@ -431,9 +431,6 @@ void vtkOSPRayRenderer::DeviceRender()
 // let the renderer display itself appropriately based on its layer index
 void vtkOSPRayRenderer::LayerRender()
 {
-  glClearColor(0,1,0,0);
-  glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
-  glClearColor(0,0,0,0);
   int     i, j;
   int     rowLength,  OSPRaySize[2];
   int     minWidth,   minHeight;
